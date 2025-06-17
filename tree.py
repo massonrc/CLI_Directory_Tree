7b728bd18debfc668e751e89441fd38c1ff02deb
Roxanne Masson <rmasson6@gatech.edu>
2025-06-17 17:43:30 +0200

    Add tree_body

--- tree.py
+++ tree.py
@@ -25,6 +25,10 @@ class DirectoryTree:
 
 class _TreeGenerator:
     def __init__(self, root_dir):
+        """
+        Constructor
+        :param root_dir:
+        """
         self._root_dir = pathlib.Path(root_dir)
         self._tree = [] # Shape the directory tree diagram
 
@@ -43,3 +47,24 @@ class _TreeGenerator:
         """
         self._tree.append(f"{self._root_dir}{os.sep}")
         self._tree.append(PIPE)
+
+    def _tree_body(self, directory, prefix=""):
+        """
+        This will add the name of the directory to the _tree
+        :param directory: Holds the path to the directory you want to walk through. A Path object.
+        :param prefix: Holds a prefix string that you use to draw the tree diagram on the terminal window
+        :return:
+        """
+        entries = directory.iterdir() # Assign result to entries, returns subdirectories in directory
+        entries = sorted(entries, key = lambda entry: entry.is_file())
+        entries_count = len(entries) # get number of entries in directory
+
+        # Loop over entries in directory, use enumerate to associate an index to each entry
+        for index, entry in enumerate(entries):
+            connector = ELBOW if index == entries_count - 1 else TEE
+            if entry.is_dir():
+                self._add_directory(
+                    entry, index, entries_count, prefix, connector
+                )
+            else:
+                self._add_file(entry, prefix, connector)
