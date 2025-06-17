0a1711819e2b5272a43d1347853ac7fc4870e832
Roxanne Masson <rmasson6@gatech.edu>
2025-06-17 15:50:21 +0200

    Initial commit

---
+++ tree.py
@@ -0,0 +1,12 @@
+"""
+This module will provide the RP Tree main module
+"""
+import os
+import pathlib
+
+# Define connector characters for tree diagram
+PIPE = "|"
+ELBOW = "'--"
+TEE = "|--"
+PIPE_PREFIX = "|   "
+SPACE_PREFIX = "    "
