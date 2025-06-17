0c36b9e135ed4ac0f51a2402c17e2992c7e72677
Roxanne Masson <rmasson6@gatech.edu>
2025-06-17 18:32:55 +0200

    Add the add_directory and add_file methods

--- tree.py
+++ tree.py
@@ -53,7 +53,7 @@ class _TreeGenerator:
         This will add the name of the directory to the _tree
         :param directory: Holds the path to the directory you want to walk through. A Path object.
         :param prefix: Holds a prefix string that you use to draw the tree diagram on the terminal window
-        :return:
+        :return: None
         """
         entries = directory.iterdir() # Assign result to entries, returns subdirectories in directory
         entries = sorted(entries, key = lambda entry: entry.is_file())
@@ -68,3 +68,31 @@ class _TreeGenerator:
                 )
             else:
                 self._add_file(entry, prefix, connector)
+
+    def _add_directory(self, directory, index, entries_count, prefix, connector):
+        """
+        Helper method
+        Returns: None
+        """
+
+        self._tree.append(f"{prefix}{connector}{directory.name}{os.sep}")
+        if index != entries_count - 1:
+            prefix += PIPE_PREFIX
+
+        else:
+            prefix += SPACE_PREFIX
+
+        # Tree_body calls itself by means of ._add_directory() until it traverses the whole directory structure
+        self._tree_body(
+            directory = directory,
+            prefix = prefix,
+        )
+        self._tree.append(prefix.rstrip())
+
+    def _add_file(self, file, prefix, connector):
+        """
+        This method will append a file entry to the directory tree list
+
+        Returns: None
+        """
+        self._tree.append(f"{prefix}{connector}{file.name}")
